64c0249a65683b054c27fd6a56f3adc50846c68f
Solace4415 <tijjtay@gmail.com>
2023-05-01 11:40:52 +0100

    changes made

--- components/FocusedStatusBar.jsx
+++ components/FocusedStatusBar.jsx
@@ -1,12 +1,10 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { StatusBar } from "react-native";
+import { useIsFocused } from "@react-navigation/core";
 
-const FocusedStatus = () => {
-  return (
-    <View>
-      <Text>FocusedStatus</Text>
-    </View>
-  );
+const FocusedStatus = (props) => {
+  const isFocused = useIsFocused();
+
+  return isFocused ? <StatusBar animated={true} {...props} /> : null;
 };
 
 export default FocusedStatus;
--- components/index.js
+++ components/index.js
@@ -1,5 +1,16 @@
 import HomeHeader from "./HomeHeader";
 import FocusedStatusBar from "./FocusedStatusBar";
 import NFTCard from "./NFTCard";
+import { NFTTitle, EthPrice, SubInfo } from "./SubInfo";
+import { CircleButton, RectButton } from "./Button";
 
-export { HomeHeader, FocusedStatusBar, NFTCard };
+export {
+  HomeHeader,
+  FocusedStatusBar,
+  NFTCard,
+  CircleButton,
+  RectButton,
+  SubInfo,
+  NFTTitle,
+  EthPrice,
+};
